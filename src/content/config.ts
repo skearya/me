87ec0885f3a221f6025a8668d6accbf78f95c64e
skearya <x@example.com>
2024-07-11 18:18:09 -0400

    first blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const blogCollection = defineCollection({
 	type: "content",
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
+		description: z.string().optional(),
 		date: z.date(),
 		tags: z.array(z.string()),
 		image: z.string().optional(),
