f1e56e9ef50e5f36863ab76384120d1f02696e69
skearya <77034153+skearya@users.noreply.github.com>
2025-02-26 19:19:13 -0500

    prettier

--- src/components/spotify/types/album.ts
+++ src/components/spotify/types/album.ts
@@ -1,136 +1,135 @@
 export interface AlbumEmbedData {
-  props: Props;
-  page: string;
-  query: Query;
-  buildId: string;
-  assetPrefix: string;
-  isFallback: boolean;
-  gssp: boolean;
-  scriptLoader: any[];
+	props: Props;
+	page: string;
+	query: Query;
+	buildId: string;
+	assetPrefix: string;
+	isFallback: boolean;
+	gssp: boolean;
+	scriptLoader: any[];
 }
 
 interface Query {
-  utm_source: string;
-  id: string;
+	utm_source: string;
+	id: string;
 }
 
 interface Props {
-  pageProps: PageProps;
-  __N_SSP: boolean;
+	pageProps: PageProps;
+	__N_SSP: boolean;
 }
 
 interface PageProps {
-  state: State;
-  config: Config;
-  _sentryTraceData: string;
-  _sentryBaggage: string;
+	state: State;
+	config: Config;
+	_sentryTraceData: string;
+	_sentryBaggage: string;
 }
 
 interface Config {
-  correlationId: string;
-  strings: Strings;
-  locale: string;
-  clientId: string;
-  restrictionId: string;
+	correlationId: string;
+	strings: Strings;
+	locale: string;
+	clientId: string;
+	restrictionId: string;
 }
 
 interface Strings {
-  en: En;
+	en: En;
 }
 
 interface En {
-  translation: Translation;
+	translation: Translation;
 }
 
-interface Translation {
-}
+interface Translation {}
 
 interface State {
-  data: Data;
-  settings: Settings;
+	data: Data;
+	settings: Settings;
 }
 
 interface Settings {
-  rtl: boolean;
-  session: Session;
-  entityContext: string;
-  clientId: string;
-  isMobile: boolean;
-  isSafari: boolean;
-  isIOS: boolean;
-  isTablet: boolean;
-  isDarkMode: boolean;
+	rtl: boolean;
+	session: Session;
+	entityContext: string;
+	clientId: string;
+	isMobile: boolean;
+	isSafari: boolean;
+	isIOS: boolean;
+	isTablet: boolean;
+	isDarkMode: boolean;
 }
 
 interface Session {
-  accessToken: string;
-  accessTokenExpirationTimestampMs: number;
-  isAnonymous: boolean;
+	accessToken: string;
+	accessTokenExpirationTimestampMs: number;
+	isAnonymous: boolean;
 }
 
 interface Data {
-  entity: AlbumEntity;
-  embeded_entity_uri: string;
-  defaultAudioFileObject: DefaultAudioFileObject;
+	entity: AlbumEntity;
+	embeded_entity_uri: string;
+	defaultAudioFileObject: DefaultAudioFileObject;
 }
 
 interface DefaultAudioFileObject {
-  passthrough: string;
+	passthrough: string;
 }
 
 export interface AlbumEntity {
-  type: string;
-  name: string;
-  uri: string;
-  id: string;
-  title: string;
-  subtitle: string;
-  coverArt: null;
-  releaseDate: null;
-  duration: number;
-  maxDuration: number;
-  isPlayable: boolean;
-  isExplicit: boolean;
-  hasVideo: boolean;
-  relatedEntityUri: string;
-  trackList: TrackList[];
-  visualIdentity: VisualIdentity;
+	type: string;
+	name: string;
+	uri: string;
+	id: string;
+	title: string;
+	subtitle: string;
+	coverArt: null;
+	releaseDate: null;
+	duration: number;
+	maxDuration: number;
+	isPlayable: boolean;
+	isExplicit: boolean;
+	hasVideo: boolean;
+	relatedEntityUri: string;
+	trackList: TrackList[];
+	visualIdentity: VisualIdentity;
 }
 
 interface VisualIdentity {
-  backgroundBase: BackgroundBase;
-  backgroundTintedBase: BackgroundBase;
-  textBase: BackgroundBase;
-  textBrightAccent: BackgroundBase;
-  textSubdued: BackgroundBase;
-  image: Image[];
+	backgroundBase: BackgroundBase;
+	backgroundTintedBase: BackgroundBase;
+	textBase: BackgroundBase;
+	textBrightAccent: BackgroundBase;
+	textSubdued: BackgroundBase;
+	image: Image[];
 }
 
 interface Image {
-  url: string;
-  maxHeight: number;
-  maxWidth: number;
+	url: string;
+	maxHeight: number;
+	maxWidth: number;
 }
 
 interface BackgroundBase {
-  alpha: number;
-  blue: number;
-  green: number;
-  red: number;
+	alpha: number;
+	blue: number;
+	green: number;
+	red: number;
 }
 
 interface TrackList {
-  uri: string;
-  uid: string;
-  title: string;
-  subtitle: string;
-  isExplicit: boolean;
-  duration: number;
-  isPlayable: boolean;
-  audioPreview: AudioPreview;
+	uri: string;
+	uid: string;
+	title: string;
+	subtitle: string;
+	isExplicit: boolean;
+	duration: number;
+	isPlayable: boolean;
+	audioPreview: AudioPreview;
 }
 
 interface AudioPreview {
-  format: string;
-  url: string;
+	format: string;
+	url: string;
 }
