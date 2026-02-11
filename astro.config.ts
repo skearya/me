19a7d3de1adefd553b9bbc969072061386990d12
skearya <77034153+skearya@users.noreply.github.com>
2026-02-10 20:14:28 -0500

    motion

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,11 @@
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import {
+	transformerNotationDiff,
+	transformerNotationFocus,
+	transformerNotationHighlight,
+} from "@shikijs/transformers";
 import { defineConfig, passthroughImageService } from "astro/config";
 import { codeLabel } from "./src/components/blog/codeLabel";
 import { removePrettierIgnore } from "./src/components/blog/removePrettierIgnore";
@@ -21,7 +26,18 @@ export default defineConfig({
 	markdown: {
 		shikiConfig: {
 			theme: "houston",
-			transformers: [removePrettierIgnore, codeLabel],
+			transformers: [
+				removePrettierIgnore,
+				codeLabel,
+				transformerNotationDiff(),
+				transformerNotationHighlight(),
+				transformerNotationFocus(),
+			],
+		},
+	},
+	vite: {
+		server: {
+			allowedHosts: [".trycloudflare.com"],
 		},
 	},
 });
